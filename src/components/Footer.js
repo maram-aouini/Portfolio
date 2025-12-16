e7f411ccea125de7e7b2cdb4a816a041895077fd
Maram Aouini <maram.aouini00@gmail.com>
2025-12-16 12:10:14 +0100

    Fix invalid anchors for Netlify CI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,35 +1,58 @@
 import { Container, Row, Col } from "react-bootstrap";
-/* import { MailchimpForm } from "./MailchimpForm"; */
 import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-items-center">
+          <Col sm={6}>
+            <img src={logo} alt="Maram Aouini logo" />
+          </Col>
 
-    return (
-        <footer className="footer">
-            <Container>
-                <Row className="align-items-center">
-                    <Col sm={6}>
-                        <img src={logo} alt="Logo" />
-                    </Col>
-                    <Col sm={6}>
-                        <div className="text-center text-sm-end">
-                            <div className="social-icon">
-                                <a href="#"><img src={navIcon1} alt="" /></a>
-                                <a href="#"><img src={navIcon2} alt="" /></a>
-                                <a href="#"><img src={navIcon3} alt="" /></a>
-                            </div>
-                        </div>
-                    </Col>
-                    <Col sm={6}>
-                        <div className="footer-bottom">
-                            <p>Copyright © 2025. All rights reserved. Maram Aouini</p>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </footer>
-    );
-}
+          <Col sm={6}>
+            <div className="text-center text-sm-end">
+              <div className="social-icon">
+                <a
+                  href="https://www.linkedin.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="LinkedIn"
+                >
+                  <img src={navIcon1} alt="LinkedIn" />
+                </a>
+
+                <a
+                  href="https://www.facebook.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Facebook"
+                >
+                  <img src={navIcon2} alt="Facebook" />
+                </a>
+
+                <a
+                  href="https://www.instagram.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label="Instagram"
+                >
+                  <img src={navIcon3} alt="Instagram" />
+                </a>
+              </div>
+            </div>
+          </Col>
+
+          <Col sm={12}>
+            <div className="footer-bottom text-center">
+              <p>Copyright © 2025. All rights reserved. Maram Aouini</p>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,18 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { HashLink } from 'react-router-hash-link';
+import { HashLink } from "react-router-hash-link";
 
-import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const NavBar = () => {
-  const [activeLink, setActiveLink] = useState('home');
+  const [activeLink, setActiveLink] = useState("home");
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
-    const onScroll = () => {
-      setScrolled(window.scrollY > 50);
-    };
-
+    const onScroll = () => setScrolled(window.scrollY > 50);
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
@@ -27,36 +24,35 @@ export const NavBar = () => {
   return (
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="/">
-          <img src={logo} alt="Logo" />
+        {/* Logo → valid anchor */}
+        <Navbar.Brand href="#home">
+          <img src={logo} alt="Maram Aouini logo" />
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
-          <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
             <Nav.Link
               href="#home"
-              className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('home')}
+              className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("home")}
             >
               Home
             </Nav.Link>
 
             <Nav.Link
               href="#skills"
-              className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('skills')}
+              className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("skills")}
             >
               Skills
             </Nav.Link>
 
             <Nav.Link
               href="#projects"
-              className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'}
-              onClick={() => onUpdateActiveLink('projects')}
+              className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("projects")}
             >
               Projects
             </Nav.Link>
@@ -64,18 +60,40 @@ export const NavBar = () => {
 
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="LinkedIn" /></a>
-              <a href="#"><img src={navIcon2} alt="Facebook" /></a>
-              <a href="#"><img src={navIcon3} alt="Instagram" /></a>
+              <a
+                href="https://www.linkedin.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="LinkedIn"
+              >
+                <img src={navIcon1} alt="LinkedIn" />
+              </a>
+
+              <a
+                href="https://www.facebook.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Facebook"
+              >
+                <img src={navIcon2} alt="Facebook" />
+              </a>
+
+              <a
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Instagram"
+              >
+                <img src={navIcon3} alt="Instagram" />
+              </a>
             </div>
 
             <HashLink smooth to="#contact">
-              <button className="vvd">
+              <button type="button" className="vvd">
                 <span>Let’s Connect</span>
               </button>
             </HashLink>
           </span>
-
         </Navbar.Collapse>
       </Container>
     </Navbar>
